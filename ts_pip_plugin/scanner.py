70068180831336a2eacfa72967e91c0a403abc38
Grigory Markin <gr.markin@gmail.com>
2020-11-26 12:54:52 +0100

    discover transitive dependencies from pip packages

--- ts_pip_plugin/scanner.py
+++ ts_pip_plugin/scanner.py
@@ -2,7 +2,9 @@ import os
 import re
 
 from pathlib import Path
-from importlib_metadata import metadata, PackageNotFoundError
+from importlib_metadata import distribution, Distribution, PackageNotFoundError
+from typing import List
+
 
 class Scanner:
     def __init__(self, client):
@@ -51,22 +53,30 @@ class Scanner:
 
 
 def create_dependencies(packages):
-
     processed_packages = set()
+    pkg_require_expr_regex = re.compile(r'([a-zA-Z0-9_\-]+)(?:.*)')
+
+    def parse_requires(requires):
+        if requires:
+            for req in requires:
+                for pkg in pkg_require_expr_regex.findall(req):
+                    yield pkg
 
     def do_create_dependencies(pkgs):
-        packages_info = []
+        packages_info: List[Distribution] = []
         for pkg in pkgs:
             try:
-                packages_info.append(metadata(pkg))
+                packages_info.append(distribution(pkg))
             except  PackageNotFoundError as err:
                 continue
 
         for info in packages_info:
-            name = info.get('name', '')
+            metadata = info.metadata
+
+            name =  metadata.get('Name', '')
             key = 'pip:' + name.lower()
 
-            version = info.get('version', None)
+            version = metadata.get('Version', None)
             if version is None:
                 versions = []
             else:
@@ -80,19 +90,21 @@ def create_dependencies(packages):
 
             if name not in processed_packages:
                 processed_packages.add(name)
-                licence = info.get('license', None)
+                licence = metadata.get('License', None)
                 if licence is None:
                     licenses = []
                 else:
                     licenses = [{'name': licence}]
 
-                dep['description'] = info.get('summary', '')
+                dep['description'] = metadata.get('Summary', '')
                 dep['private']  = False
-                dep['homepageUrl'] = info.get('home-page', '')
-                dep['repoUrl'] = ''
+                dep['homepageUrl'] = metadata.get('Home-page', '')
+                dep['repoUrl'] = metadata.get('Download-URL', '')
                 dep['checksum'] = ''
                 dep['licenses'] = licenses
-                dep['dependencies'] = [d for d in do_create_dependencies(info.get('requires', []))]
+
+                deps = [pkg for pkg in parse_requires(info.requires)]
+                dep['dependencies'] = [d for d in do_create_dependencies(deps)]
 
             yield dep
 
